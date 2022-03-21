2e7b5f14d2f532a6a8c0f5ddfa5086741e07a1a2
ck <2658868303@qq.com>
2022-03-21 12:45:10 +0800

    master commit2

--- src/main/java/com/zk/git/GitTest.java
+++ src/main/java/com/zk/git/GitTest.java
@@ -12,5 +12,6 @@ public class GitTest {
         System.out.println("Hello git6");
         System.out.println("master test");
         System.out.println("hot-fix");
+        System.out.println("push commit");
     }
 }
