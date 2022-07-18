e64bd4205e0bf905d2692016372b572fbe0d0aa6
ck <2658868303@qq.com>
2022-07-18 13:38:46 +0800

    7月18日提交的内容,复习一下提交代码的步骤

--- src/main/java/com/zk/git/GitTest.java
+++ src/main/java/com/zk/git/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
         System.out.println("hot-fix");
         System.out.println("push commit");
         System.out.println("pull commit");
+        System.out.println("pull commit");
     }
 }
