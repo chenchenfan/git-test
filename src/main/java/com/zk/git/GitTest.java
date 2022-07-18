70c5afabaeacdbab537ee52b83ae27e174c0d02d
ck <2658868303@qq.com>
2022-07-18 13:50:48 +0800

    7月18日提交的内容,复习一下提交代码的步骤,测试合并冲突

--- src/main/java/com/zk/git/GitTest.java
+++ src/main/java/com/zk/git/GitTest.java
@@ -12,6 +12,7 @@ public class GitTest {
         System.out.println("Hello git6");
         System.out.println("master test");
         System.out.println("hot-fix");
+        System.out.println("冲突");
 
 
     }
